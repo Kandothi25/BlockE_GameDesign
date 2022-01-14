e227b17aad754c350d8a6c048521bef4be34456e
Kandothi25 <toxicrypt3658@gmail.com>
2022-01-14 10:02:31 -0600

    Today's class 1/14/22

---
+++ average.py
@@ -0,0 +1,22 @@
+import os
+# Ishaan Kandoth
+# 1/14/22
+# Declare variables, print variables, print type of data, learn some operators
+
+# This symbol is for comments, means the computer will ignore
+os.system('cls')
+# Program is Average of 3 tests
+
+#Declare and 
+test1 = 89
+test2 = 78.5
+test3 = 86
+Flag = False
+
+# To display on the screen we can use the function print
+# print(type(test1), type(test2), type(Flag))
+sum = test1+test2+test3
+print('The sum is', sum)
+Average = sum/3
+print('The average is', Average)
+print('Hello world')
