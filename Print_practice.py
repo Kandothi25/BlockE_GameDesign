ff180a86312ca3b69612f05476b3d7ee3ea74507
Kandothi25 <toxicrypt3658@gmail.com>
2022-01-20 14:17:33 -0600

    Homework for 1/21/22

---
+++ Print_practice.py
@@ -0,0 +1,12 @@
+#Question 1:
+print('Greenhill : 10, Portal : 5.33')
+print('Total students : 240, Boys : 120')
+print('031')
+print('3.561E+02')
+
+#Question 2:
+print('Welcome to', end=' ')
+print('Greenhill School')
+
+#Question 3:
+print('I\nlove\t\"Greenhill\"')
