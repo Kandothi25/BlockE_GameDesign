18d8e2e2977df680165abb55a169ac088ade786d
Kandothi25 <toxicrypt3658@gmail.com>
2022-01-25 18:10:36 -0600

    1/25/22

--- NumberGussingGame_ver3.py
+++ NumberGussingGame_ver3.py
@@ -36,12 +36,21 @@ while(RunGame):
             print('Sorry, not an option, please enter \"1\", \"2\" or \"3\" only')
     if int(Gamemode)==1:
         myNumber=random.randint(1,10)
+        attempts=5
     elif int(Gamemode)==2:
         myNumber=random.randint(1,50)
+        attempts=10
     elif int(Gamemode)==3:
         myNumber=random.randint(1,100)
+        attempts=15
+    #print('The number is', myNumber) #For testing the code
     GameOn=True
     while(GameOn):
+        if attempts==1:
+            GameOn=False
+        else:
+            GameOn=True
+        print('You have ',attempts,' attempts left. Good luck!')
         userGuess=input('Guess the number: ')
         if str.isnumeric(userGuess):
             if int(myNumber) == int(userGuess):
@@ -49,17 +58,20 @@ while(RunGame):
                 GameOn=False
             elif int(myNumber)>int(userGuess):
                 print('Incorrect! Too low.\nIf you give up, type \"quit\" to end the game.')
+                attempts=attempts-1
             elif int(myNumber)<int(userGuess):
                 print('Incorrect! Too high.\nIf you give up, type \"quit\" to end the game.')
+                attempts=attempts-1
         elif str(userGuess) == 'quit':
             print('Better luck next time!')
             GameOn=False
         else:
             print('Incorrect! If you give up, type \"quit\" to end the game.')
+            attempts=attempts-1
     print('The number was', myNumber)
     restart=input('Would you like to play again? Type \"yes\" or \"no\"\n')
     if restart==str('yes'):
-        print('restarting')
+        print('restarting...')
     elif restart==str('no'):
         RunGame=False
     os.system('cls')
