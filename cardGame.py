5cbeb985001bc4b6389729628c836954fd04adf1
Kandothi25 <toxicrypt3658@gmail.com>
2022-02-17 18:14:07 -0600

    2/17/22

--- cardGame.py
+++ cardGame.py
@@ -6,10 +6,10 @@ os.system('cls')
 numberCards=[]
 for i in range(2,11):
     numberCards.append(i)
+numberCards.insert(0,'A')
 suits=['♠', '♦', '♣', '♥']
-royals=['J', 'Q', 'K', 'A']
-#Make a combined list [2,3,4,5,6,7,8,9,10,11,12,J,Q,K, etc...]
-#Make combinations for every card ex: 2c, 2h, 5s ...
+royals=['J', 'Q', 'K']
+#Make a combined list for all cards [A♠,A♦,A♣,A♥,2♠,2♦,2♣,2♥,...J,Q,K, etc...]
 finalDeck=[]
 def Deck(cardnumber):
     global suits
@@ -22,4 +22,4 @@ def Deck(cardnumber):
 for l in range(0,8):
     Deck(l)
 finalDeck.extend(royals)
-print('Deck of Cards:\n\n',finalDeck)
+print('Deck of Cards:\n\n'+str(finalDeck))
