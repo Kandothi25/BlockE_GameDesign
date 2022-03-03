66bf95636bdfe15f0217c4b416ef75879c2a3e5e
Kandothi25 <toxicrypt3658@gmail.com>
2022-03-03 17:47:54 -0600

    3/3/22

---
+++ colors.py
@@ -0,0 +1,15 @@
+#Ishaan Kandoth
+#3/2/22
+import os, pygame
+os.system('cls')
+#hex codes for colors
+red=[255,0,0]
+orange=[255,165,0]
+yellow=[255,255,0]
+green=[0,255,00]
+blue=[0,0,255]
+cyan=[0,255,255]
+purple=[128,0,128]
+magenta=[255,0,255]
+black=[0,0,0]
+white=[255,255,255]
