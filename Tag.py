3dc696f9ec20a9cc098366cf851cc92f43893f38
Kandothi25 <toxicrypt3658@gmail.com>
2022-03-09 18:17:08 -0600

    3/9/22

--- Tag.py
+++ Tag.py
@@ -3,8 +3,7 @@
 #draw circles and rectangles
 #use keys to move objects
 #use dictionaries
-import os, time, random, pygame
-from turtle import circle
+import os, random, pygame
 #Objective is for rectangle to run away from circle
 #If they colide, circle eats the rectangle and gets larger
 #Rectangle restarts
@@ -31,6 +30,12 @@ yc=random.randint(rad,HEIGHT-rad)
 #creating square
 square=pygame.Rect(xs,ys,wbox,hbox)
 
+#circle hitbox
+c_wbox=rad
+c_hbox=rad
+xh=xc-(rad/2)
+yh=yc-(rad/2)
+hitbox=pygame.Rect(xh,yh,c_wbox,c_hbox)
 #creating screen
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Tag')
@@ -44,6 +49,7 @@ colors={'red':[255,0,0],'orange':[255,165,0],'yellow':[255,255,0],'green':[0,255
 background=colors.get('black')
 sq_color=colors.get('red')
 cr_color=colors.get('blue')
+hb_color=colors.get('blue')
 
 while check:
     screen.fill(background)
@@ -51,6 +57,7 @@ while check:
         if i.type==pygame.QUIT:
             check=False
     keys=pygame.key.get_pressed() #this returns a list
+    #movement for square
     if keys[pygame.K_LEFT] and square.x>=move:
         square.x-=move
     if keys[pygame.K_RIGHT] and square.x<=WIDTH-hbox:
@@ -59,17 +66,34 @@ while check:
         square.y-=move
     if keys[pygame.K_DOWN] and square.y<=HEIGHT-wbox:
         square.y+=move
-    
-    if keys[pygame.K_a] and xc>=rad:
+    #movement for circle
+    if keys[pygame.K_a] and xc>=move:
         xc-=move
-    if keys[pygame.K_d] and xc<=WIDTH-rad:
+        hitbox.x-=move
+    if keys[pygame.K_d] and xc<=WIDTH-move:
         xc+=move
-    if keys[pygame.K_w] and yc>=rad:
+        hitbox.x+=move
+    if keys[pygame.K_w] and yc>=move:
         yc-=move
-    if keys[pygame.K_s] and yc<=HEIGHT-rad:
+        hitbox.y-=move
+    if keys[pygame.K_s] and yc<=HEIGHT-move:
         yc+=move
-#Finish circle
+        hitbox.y+=move
+    #collisions
+    if square.colliderect(hitbox):
+        xs=random.randint(0,WIDTH-wbox)
+        ys=random.randint(0,HEIGHT-hbox)
+        square=pygame.Rect(xs,ys,wbox,hbox)
+        rad+=10
+        c_wbox+=10
+        c_hbox+=10
+        xh=xc-(rad/2)
+        yh=yc-(rad/2)
+        hitbox=pygame.Rect(xh,yh,c_wbox,c_hbox)
+
+    #Finish circle
     pygame.draw.rect(screen, sq_color, square)
     pygame.draw.circle(screen, cr_color, (xc,yc), rad)
+    pygame.draw.rect(screen, hb_color, hitbox)
     pygame.display.update()
-    pygame.time.delay(1)
+    pygame.time.delay(3)
