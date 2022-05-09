6a099a4b12145d4c281656a3f5fdfaafc34a13f6
Kandothi25 <toxicrypt3658@gmail.com>
2022-05-08 22:41:26 -0500

    5/8/22

--- FinalGame/Level3.py
+++ FinalGame/Level3.py
@@ -13,6 +13,17 @@ jump_move=20
 full_health=True
 medium_health=False
 low_health=False
+bossLeft=False
+bossRight=False
+bossMove=True
+bossReturnLeft=False
+bossReturnRight=False
+projectilesOn=False
+projectilesActive=False
+projectilesLeft=False
+projectilesRight=False
+
+bossNewPos=0
 
 colors={'red':[255,0,0],'orange':[255,165,0],'yellow':[255,255,0],'green':[0,255,00],
 'blue':[0,0,255],'purple':[128,0,128],'cyan':[0,255,255],'magenta':[255,0,255],
@@ -25,7 +36,9 @@ xt=WIDTH/2-Health_title.get_width()/2
 char_color=colors.get('blue')
 char_side_color=colors.get('cyan')
 boss_color=colors.get('red')
-projectile_color=colors.get('yellow')
+projectile_color1=colors.get('purple')
+projectile_color2=colors.get('magenta')
+projectile_color3=colors.get('cyan')
 health_color3=colors.get('green')
 health_color2=colors.get('yellow')
 health_color1=colors.get('red')
@@ -62,9 +75,11 @@ character_bottom=pygame.Rect(xc_b,yc_b,char_bottom_wb,char_bottom_hb)
 #boss variables
 boss_hb=60
 boss_wb=60
-xb=WIDTH/2
+xb=WIDTH/2-boss_wb/2
 yb=HEIGHT-110
 boss=pygame.Rect(xb,yb,boss_wb,boss_hb)
+
+bossPhase=random.randint(1,2)
 
 #boss health bar variables
 
@@ -90,11 +105,23 @@ healthPoint3=pygame.Rect(xhp3,yhp3,hp3_wb,hp3_hb)
 #projectile variables
 
 #projectile 1
-proj_hb=10
-proj_wb=10
-xp=xb-proj_wb
-yp=yb+25
-projectile1=pygame.Rect(xp,yp,proj_wb,proj_hb)
+proj1_hb=20
+proj1_wb=20
+xp1=xb-proj1_wb
+yp1=yb+20
+projectile1=pygame.Rect(xp1,yp1,proj1_wb,proj1_hb)
+#projectile 2
+proj2_hb=20
+proj2_wb=20
+xp2=xb-proj2_wb
+yp2=yb+20
+projectile2=pygame.Rect(xp2,yp2,proj2_wb,proj2_hb)
+#projectile 3
+proj3_hb=20
+proj3_wb=20
+xp3=xb-proj3_wb
+yp3=yb+20
+projectile3=pygame.Rect(xp3,yp3,proj3_wb,proj3_hb)
 
 background=pygame.transform.scale(pygame.image.load('FinalGame\Map_DemonIsland.png'),(1920,1080))
 xbg=-615
@@ -129,18 +156,162 @@ while playLvl3:
             jumping=False
             jump_move=20
     
+    if bossPhase==1:
+        bossMove=True
+        if bossMove:
+            if character.x<boss.x:
+                bossLeft=True
+                bossRight=False
+                bossPhase=0
+            if character.x>boss.x:
+                bossRight=True
+                bossLeft=False
+                bossPhase=0
+    
+    if bossPhase==2:
+        print('Phase2')
+        if boss.x>WIDTH/2-boss_wb/2:
+            bossReturnLeft=False
+            bossReturnRight=True
+        if boss.x<WIDTH/2-boss_wb/2:
+            bossReturnRight=False
+            bossReturnLeft=True
+        if bossLeft:
+            bossLeft=False
+            bossReturnLeft=True
+        if bossRight:
+            bossRight=False
+            bossReturnRight=True
+        if bossReturnLeft:
+            print('boss is returning from left')
+            boss.x+=move
+        if bossReturnRight:
+            print('boss is returning from right')
+            boss.x-=move
+        if boss.x==WIDTH/2-boss_wb/2:
+            print('boss has returned')
+            bossReturnLeft=False
+            bossReturnRight=False
+            projectilesOn=True
+            bossPhase=0
+    
+    if projectilesOn:
+        if character.x<boss.x:
+            projectile1.x=xb-proj1_wb
+            projectile2.x=xb-proj2_wb
+            projectile3.x=xb-proj3_wb
+            projectilesLeft=True
+        if character.x>boss.x:
+            projectile1.x=xb+boss_wb
+            projectile2.x=xb+boss_wb
+            projectile3.x=xb+boss_wb
+            projectilesRight=True
+        projectilesActive=True
+        projectilesOn=False
+
+    if projectilesActive:
+        if projectilesLeft:
+            projectile1.x-=move
+            if projectile1.x+proj1_wb<0:
+                projectile2.x-=move
+            if projectile2.x+proj2_wb<0:
+                projectile3.x-=move
+            if projectile3.x+proj3_wb<0:
+                projectilesLeft=False
+                projectilesActive=False
+                bossPhase=random.randint(1,2)
+        if projectilesRight:
+            projectile1.x+=move
+            if projectile1.x-proj1_wb>WIDTH:
+                projectile2.x+=move
+            if projectile2.x-proj2_wb>WIDTH:
+                projectile3.x+=move
+            if projectile3.x-proj3_wb>WIDTH:
+                projectilesRight=False
+                projectilesActive=False
+                bossPhase=random.randint(1,2)
+
+    if bossLeft:
+        print('phase1')
+        boss.x-=move
+        if boss.x<=0:
+            bossPhase=random.randint(1,2)
+    if bossRight:
+        print('phase1')
+        boss.x+=move
+        if boss.x+boss_wb>=WIDTH:
+            bossPhase=random.randint(1,2)
+
+    if bossNewPos==1:
+        boss.x=0+boss_wb
+        bossPhase=random.randint(1,2)
+        bossNewPos=0
+    if bossNewPos==2:
+        boss.x=WIDTH-boss_wb*2
+        bossPhase=random.randint(1,2)
+        bossNewPos=0
+
     if character_left.colliderect(boss) or character_right.colliderect(boss):
         playLvl3=False
         print('Game Over\nYou lose')
+    if projectilesOn or projectilesActive:
+        if character.colliderect(projectile1) or character.colliderect(projectile2) or character.colliderect(projectile3):
+            playLvl3=False
+            print('Game Over\nYou lose')
     
     if character_bottom.colliderect(boss) and full_health==True:
+        print('e')
         full_health=False
         medium_health=True
-        boss.x=0
+        bossPhase=0
+        bossLeft=False
+        bossRight=False
+        bossReturnLeft=False
+        bossReturnRight=False
+        projectilesActive=False
+        projectilesOn=False
+        projectilesLeft=False
+        projectilesRight=False
+        if boss.x==WIDTH/2-boss_wb/2:
+            bossNewPos=random.randint(1,2)
+        else:
+            if boss.x>WIDTH/2-boss_wb/2:
+                bossNewPos=1
+            if boss.x<WIDTH/2-boss_wb/2:
+                bossNewPos=2
+        if bossNewPos==1:
+            boss.x=0+boss_wb
+            bossPhase=random.randint(1,2)
+            bossNewPos=0
+        if bossNewPos==2:
+            boss.x=WIDTH-boss_wb*2
+            bossPhase=random.randint(1,2)
+            bossNewPos=0
     if character_bottom.colliderect(boss) and medium_health==True:
+        print('ee')
         medium_health=False
         low_health=True
-        boss.x=WIDTH-boss_wb
+        bossPhase=0
+        bossLeft=False
+        bossRight=False
+        bossReturnLeft=False
+        bossReturnRight=False
+        if boss.x==WIDTH/2-boss_wb/2:
+            bossNewPos=random.randint(1,2)
+        else:
+            if boss.x>WIDTH/2-boss_wb/2:
+                bossNewPos=1
+            if boss.x<WIDTH/2-boss_wb/2:
+                bossNewPos=2
+        if bossNewPos==1:
+            boss.x=0+boss_wb
+            bossPhase=random.randint(1,2)
+            bossNewPos=0
+        if bossNewPos==2:
+            boss.x=WIDTH-boss_wb*2
+            bossPhase=random.randint(1,2)
+            bossNewPos=0
+
     if character_bottom.colliderect(boss) and low_health==True:
         playLvl3=False
         print('Game Over\nYou win')
@@ -165,5 +336,9 @@ while playLvl3:
         pygame.draw.rect(screen,health_gone,healthPoint2)
         pygame.draw.rect(screen,health_gone,healthPoint3)
     #pygame.draw.rect(screen,projectile_color,)
+    if projectilesOn or projectilesActive:
+        pygame.draw.rect(screen,projectile_color1,projectile1)
+        pygame.draw.rect(screen,projectile_color2,projectile2)
+        pygame.draw.rect(screen,projectile_color3,projectile3)
     pygame.time.delay(20)
     pygame.display.update()
